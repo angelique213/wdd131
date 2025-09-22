bff73f2f602250fe1664506a19e10719a4a4c2ad
angelique213 <angeliquelegaspi123@gmail.com>
2025-09-22 17:26:54 -0600

    update it

--- prove/mission/mission.js
+++ prove/mission/mission.js
@@ -5,10 +5,10 @@ const logo = document.querySelector('.logo');
 function changeTheme() {
     if (themeSelector.value === 'dark') {
         body.classList.add('dark');
-        logo.src = 'byui-logo-white.png'; 
-    } else {
+        logo.src = 'byuilogo-white.png';
+    } else if (themeSelector.value === 'light') {
         body.classList.remove('dark');
-        logo.src = 'byuilogo.png';       
+        logo.src = 'byuilogo.png';
     }
 }
 
